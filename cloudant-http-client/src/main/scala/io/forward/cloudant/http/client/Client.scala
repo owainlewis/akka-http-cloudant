00a10a4c667707e158b0a32f6c6ce3da89f31b60
Owain Lewis <owain@owainlewis.com>
2016-06-22 16:59:53 +0100

    Update Client.scala

--- cloudant-http-client/src/main/scala/io/forward/cloudant/http/client/Client.scala
+++ cloudant-http-client/src/main/scala/io/forward/cloudant/http/client/Client.scala
@@ -23,9 +23,7 @@ final class Client(config: CloudantConfig) {
   val search = new SearchOperations(config)
   val view = new ViewOperations(config)
 
-  def run[T](req: HttpRequest)(implicit
-                               ec: ExecutionContext,
-                               um: Unmarshaller[ResponseEntity, T]): Future[CloudantResponse[T]] =
+  def run[T](req: HttpRequest)(implicit ec: ExecutionContext, um: Unmarshaller[ResponseEntity, T]): Future[CloudantResponse[T]] =
     runRequest(req) flatMap { response =>
       Unmarshal(response.entity.withContentType(ContentTypes.`application/json`)).to[T] map { body =>
         CloudantResponse(response.status.intValue, body)
